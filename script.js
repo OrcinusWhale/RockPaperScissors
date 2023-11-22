cacba6955fb5b0835f0e0c92112662ab61816ff4
OrcinusWhale <or.barer@gmail.com>
2023-11-22 19:52:24 +0200

    Basic js game with 5 rounds implemented in console

---
+++ script.js
@@ -0,0 +1,42 @@
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"]
+    return choices[Math.floor(Math.random()*3)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return `It's a draw! You both chose ${computerSelection}`
+    }
+    else if (playerSelection.toLowerCase() == "rock"){
+        if (computerSelection == "Paper") {
+            return `You lose! Paper beats Rock`
+        }
+        else {
+            return `You win! Rock beats Scissors`
+        }
+    }
+    else if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection == "Rock") {
+            return `You win! Paper beats Rock`
+        }
+        else {
+            return `You lose! Scissors beat paper`
+        }
+    }
+    else {
+        if (computerSelection == "Rock") {
+            return `You lose! Rock beats Scissors`
+        }
+        else {
+            return `You win! Scissors beat Paper`
+        }
+    }
+}
+
+function game() {
+    let playerSelection;
+    for (let i=0; i<5; ++i) {
+        playerSelection = prompt("Please enter your selection (Rock, Paper, Scissors): ")
+        console.log(playRound(playerSelection, getComputerChoice()))
+    }
+}
