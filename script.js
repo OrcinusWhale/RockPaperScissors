f4cbde08ec8c5793869aa05b42537f1619fe32e0
OrcinusWhale <or.barer@gmail.com>
2023-12-04 15:00:30 +0200

    finished making gui

--- script.js
+++ script.js
@@ -9,35 +9,66 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection.toLowerCase() == "rock"){
         if (computerSelection == "Paper") {
+            cpuScore++;
             return `You lose! Paper beats Rock`
         }
         else {
+            playerScore++;
             return `You win! Rock beats Scissors`
         }
     }
     else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection == "Rock") {
+            playerScore++;
             return `You win! Paper beats Rock`
         }
         else {
+            cpuScore++;
             return `You lose! Scissors beat paper`
         }
     }
     else {
         if (computerSelection == "Rock") {
+            cpuScore++;
             return `You lose! Rock beats Scissors`
         }
         else {
+            playerScore++;
             return `You win! Scissors beat Paper`
         }
     }
 }
 
+function displayWinner(message) {
+    msg = document.querySelector("#message");
+    if (playerScore == 5) {
+        msg.innerText = "YOU WIN!"
+    }
+    else if(cpuScore == 5) {
+        msg.innerText = "YOU LOSE!"
+    }
+    if (playerScore == 5 || cpuScore == 5) {
+        selectionBtns.forEach(button => { 
+            button.removeEventListener("click", calls[button.alt]);
+            button.style.opacity = 0.5;
+        })
+    }
+    else {
+        msg.innerText = message;
+    }
+    document.querySelector("#player-score").innerText = `You: ${playerScore}`;
+    document.querySelector("#cpu-score").innerText = `CPU: ${cpuScore}`;
+}
+
 let playerScore = 0;
 let cpuScore = 0;
+const calls = {
+    "rock" : undefined,
+    "paper" : undefined,
+    "scissors" : undefined
+};
 const selectionBtns = document.querySelectorAll(".selection");
-selectionBtns.forEach(button => 
-    button.addEventListener("click", () => 
-        console.log(playRound(button.textContent, getComputerChoice()))
-    )
-);
+selectionBtns.forEach(button => {
+    calls[button.alt] = () => displayWinner(playRound(button.alt, getComputerChoice()));
+    button.addEventListener("click", calls[button.alt]);
+});
